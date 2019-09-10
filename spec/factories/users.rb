482663a1f57ba1241746b359912f1d4161bac2b8
Hiroki1888999 <hkaneuchi80@gmail.com>
2019-09-10 19:06:35 +0900

    write-test-code-for-user-registration

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
     email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    password              {"0000aaaa"}
+    password_confirmation {"0000aaaa"}
   end
 
 end
