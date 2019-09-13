f5c38c6a1df16b12f219a158272211e49ee3bba3
Hiroki1888999 <hkaneuchi80@gmail.com>
2019-09-13 17:58:44 +0900

    edit routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def show
   end
+  def edit
+  end
+  def sign_out
+  end
+  def profile
+  end
+  def deliver_address
+  end
+  def card
+  end
+  def email_password
+  end
+  def identification
+  end
+  def sms_confirmation
+  end
 end
