bf51bb274047d20676e9d90fee7388e3343afbc3
Tatsuka <[tatsuka.m18@gmail.com]>
2019-09-17 16:00:45 +0900

    Itemsテーブル追加分①

--- db/migrate/20190917051707_add_items.rb
+++ db/migrate/20190917051707_add_items.rb
@@ -1,18 +1,13 @@
 class AddItems < ActiveRecord::Migration[5.2]
   def change
+    add_column :items, :name, :string, null: false
+    add_column :items, :detail, :text, null: false
+    add_column :items, :shipping_cost, :integer, null: false
+    add_column :items, :shipping_fee_charge_to, :string, null: false
+    add_column :items, :shipping_from, :integer, null: false
+    add_column :items, :shipping_days, :string, null: false
+    add_column :items, :category, :references, null: false, foreign_key:true
+    add_column :items, :trade_status, :references, null: false, foreign_key:true
+    add_column :items, :brand, :references, foreign_key:true
   end
 end
-
-
-
-|name|string|null: false|
-# |price|integer|null: false|
-|detail|text|null: false|
-|shipping_cost|integer|null: false|
-|shipping_fee_charge_to|integer|null: false|
-|shipping_from|integer|null: false|
-|shipping_days|integer|null: false|
-# |condition|references|null:false,foreign_key: true|
-|category|references|null:false,foreign_key:true|
-|trade_status|references|null:false,foreign_key:true|
-|brand|references|foreign_key: true|
