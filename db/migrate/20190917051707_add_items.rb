13d720b4b5e701a230f64aaf3007bf5f492c2b4d
Tatsuka <[tatsuka.m18@gmail.com]>
2019-09-17 19:46:33 +0900

    nameエラー対応

--- db/migrate/20190917051707_add_items.rb
+++ db/migrate/20190917051707_add_items.rb
@@ -1,6 +1,6 @@
 class AddItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :items, :name, :string, null: false
+    # add_column :items, :name, :string, null: false
     add_column :items, :detail, :text, null: false
     add_column :items, :shipping_cost, :integer, null: false
     add_column :items, :shipping_fee_charge_to, :string, null: false
