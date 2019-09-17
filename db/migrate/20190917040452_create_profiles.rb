8a66202e65263953aba7e4d5ab17b92137bc60c7
Tatsuka <[tatsuka.m18@gmail.com]>
2019-09-17 13:21:44 +0900

    Profileモデルとマイグレーションファイルの作成

---
+++ db/migrate/20190917040452_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.text         :introduction, null:false
+      t.string       :avatar, null:false
+      t.string       :first_name_kana, null:false
+      t.string       :last_name_kana, null:false
+      t.string       :first_name_kanji, null:false
+      t.string       :last_name_kanji, null:false
+      t.integer      :birth_year, null:false
+      t.integer      :birth_month, null:false
+      t.integer      :birth_day, null:false
+      t.references   :user, null:false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
