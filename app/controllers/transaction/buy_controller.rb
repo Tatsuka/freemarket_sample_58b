86a92135ad74ecb4af04574eab332a1946be4149
YoheiImoto <asakuranemy944@gmail.com>
2019-09-28 19:33:38 +0900

    pull_pequest_push1

--- app/controllers/transaction/buy_controller.rb
+++ app/controllers/transaction/buy_controller.rb
@@ -1,5 +1,7 @@
 class Transaction::BuyController < ApplicationController
 
+  before_action :set_item,only:[:show,:pay]
+
   def show
     @item = Item.find(params[:id])
   end
@@ -10,7 +12,7 @@ class Transaction::BuyController < ApplicationController
   def pay
     @item = Item.find(params[:id])
     @item.trade_status_id = 2
-    @item.save
+    @item.update
     Payjp.api_key = 'sk_test_1721473b3cfd0970b82ecfda'
     charge = Payjp::Charge.create(
     amount: @item.shipping_cost,
@@ -20,3 +22,7 @@ class Transaction::BuyController < ApplicationController
   end
 
 end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
