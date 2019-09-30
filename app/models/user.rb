6b0c2d0fd21580c007c213ad0b9ad8441f46c46e
Hiroki1888999 <hkaneuchi80@gmail.com>
2019-09-30 15:33:48 +0900

    edit gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
 devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable
+       :recoverable, :rememberable, :validatable, :omniauthable
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
