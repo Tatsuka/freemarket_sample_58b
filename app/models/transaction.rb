af87ad1dba7c1e14b3e36bb68fe026d119c655ed
Tatsuka <[tatsuka.m18@gmail.com]>
2019-10-01 14:24:45 +0900

    status.rbの追加

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :item
   belongs_to :transaction_partner
 
-  #ActiveHash実装時記述
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  #   belongs_to_active_hash :status
+  # ActiveHash実装時記述
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :status
 end
