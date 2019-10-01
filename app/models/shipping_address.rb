a03a24e44fd8330d400f263746dce0dca6dc8054
Tatsuka <[tatsuka.m18@gmail.com]>
2019-10-01 14:25:59 +0900

    shippingAddress.rbのactive_hash有効化

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,5 +1,5 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :user
-  # extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
