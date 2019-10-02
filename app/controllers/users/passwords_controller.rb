b126ff4f81350d4b7da584f73dcc5005a9e0fca8
shinohara <shinohello0420@yahoo.co.jp>
2019-10-02 20:20:18 +0900

    微調整

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,12 +2,15 @@ class Users::PasswordsController < ApplicationController
   def new
     @user = current_user
   end
+
   def edit
     @user = current_user
   end
+
   def update
     @user = current_user
   end
+  
   def create
     @user = current_user
   end
